159ef7c22288c8e71ed8f5140e72809c43352288
Mick Redman <guitarbeerchocolate@googlemail.com>
2012-08-29 16:05:08 +0100

    Added the README.md

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'vimeo.class.php';
+
+$addr1 = 'http://vimeo.com/api/v2/channel/videoschool/videos.xml';
+$addr2 = 'http://vimeo.com/api/v2/channel/wineaftercoffee/videos.xml';
+
+$vimeo = new vimeo;
+$vimeo->addFeed($addr1);
+$vimeo->addFeed($addr2);
+
+// $arr = $vimeo->getFeed();
+// $arr = $vimeo->getFeed('water');
+
+// $searchArray = Array('glass', 'trix');
+// $arr = $vimeo->getFeed($searchArray);
+
+foreach($arr as $row)
+{
+	echo '<iframe src="http://player.vimeo.com/video/'.$row->id.'" width="WIDTH" height="HEIGHT" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><br />';	
+}
+?>
